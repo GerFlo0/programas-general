d59554ad0dff2527c29986127e9dcffd0742f950
GerFlo0 <166448845+GerFlo0@users.noreply.github.com>
2025-02-10 15:45:24 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+import pygame
+
+ship_img = pygame.image.load('player_ship')
+ship_img.convert()
---
+++ ship.py
@@ -0,0 +1,17 @@
+import pygame
+
+class Player_ship():
+    def __init__(self,x,y):
+        self.shape = pygame.Rect(0,0,20,20)
+        self.shape.center = (x,y)
+    
+    def draw(self,window):
+        pygame.draw.rect(window,(255,255,0), self.shape)
+
+class Enemy_ship():
+    def __init__(self,x,y):
+        self.shape = pygame.Rect(0,0,20,20)
+        self.shape.center = (x,y)
+    
+    def draw(self,window):
+        pygame.draw.rect(window,(255,255,0), self.shape)
