24cedfb0d6660da0c87a9ec093f731bb0e3374b9
GerFlo0 <gerardoft20@gmail.com>
2025-02-11 20:21:40 -0600

    programa 5 gabriel

---
+++ tec/prog_logica_y_funcional/tarea5.py
@@ -0,0 +1,38 @@
+#cuatro paredes
+#pared 1 normal
+#pared 2 y 3 una ventana cada una
+#pared 4 una puerta
+#medidas de la puerta 1 x 1.9 m
+#ventanas largo 1.2m ancho 1.5m
+
+#Entradas
+#ancho del frente
+#lateral
+#altura
+
+#Constantes
+BLOCK = {
+    "lenght" : 0.39,
+    "height" : 0.2
+}
+JOINT = {
+    "lenght" : 0.01,
+    "height" : 0.015
+}
+DOOR_AREA = (1 * 1.9)
+WINDOW_AREA = (1.2 * 1.5)
+
+#Variables
+room = {
+    "width" : float(input("Ingrese el ancho del frente: ")),
+    "lenght" : float(input("Ingrese el ancho del lateral: ")),
+    "height" : float(input("Ingrese la altura del cuarto: "))
+}
+
+#Formula
+walls_area_no_holes = 2 * (room["width"] * room["height"]) + 2 * (room["lenght"] * room["height"])
+walls_area = walls_area_no_holes - (DOOR_AREA + 2 * WINDOW_AREA)
+
+blocks_total = ( walls_area ) / ( (BLOCK["lenght"] + JOINT["lenght"]) * (BLOCK["height"] + JOINT["height"]) )
+
+print(f"Para este cuarto de {"{:.2f}".format(walls_area)} metros cuadrados se ocupan {"{:.2f}".format(blocks_total)} blocks")
