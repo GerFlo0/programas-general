48fc13b5f442d1a0bce0124d3bc3513303198d86
GerFlo0 <gerardoft20@gmail.com>
2025-02-11 20:33:17 -0600

    programa techo

---
+++ tec/prog_logica_y_funcional/tarea6.py
@@ -0,0 +1,39 @@
+#material para el techo de una casa
+
+#constantes
+#espesor del techo 0.1m
+#para cada m3 de concreto se ocupa 350kg de cemento, 0.56m3 de arena, 0.84m3 de grava,  180 L de agua
+
+#entradas
+#ancho del frente
+#ancho del lateral
+
+#salida
+#material para el techo
+
+#Constantes
+ROOF_HEIGHT = 0.1
+MATERIALS_PER_CUB_METER = {
+    "cement" : 350/50,
+    "sand" : 0.56,
+    "gravel" : 0.84,
+    "water" : 180
+}
+
+#variables
+roof = {
+    "length" : float(input("Ingrese el ancho del cuarto: ")),
+    "width" : float(input("Ingrese el frente del cuarto: "))
+}
+
+floor_area = roof["length"] * roof["width"]
+floor_volume = floor_area * ROOF_HEIGHT
+
+materials_total = {k: v*floor_volume for k,v in list(zip(MATERIALS_PER_CUB_METER.keys(), MATERIALS_PER_CUB_METER.values()))}
+
+#salida
+print(f"""Para este techo de {floor_area} metros cuadrados se necesitaran los siguientes materiales
+    Cemento: {"{:.2f}".format(materials_total['cement'])} bultos de 50kg
+    Arena: {"{:.2f}".format(materials_total['sand'])} metros cubicos
+    Grava: {"{:.2f}".format(materials_total['gravel'])} metros cubicos
+    Agua: {"{:.2f}".format(materials_total['water'])} litros""")
